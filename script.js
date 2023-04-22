1fbd0540ac253725314e5965fc1d6cdd2adee6c7
arunraj3 <arunrajgovindan2002@gmail.com>
2023-04-23 04:34:39 +0530

    Version 1

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let weather = {
     fetchWeather : function(city){
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q="
-            +city
+            + city
             + "&units=metric&appid="
             + weather.apiKey
             )
@@ -16,13 +16,30 @@ let weather = {
        const {description,icon} = data.weather[0];
        const {temp,humidity} = data.main;
        const {speed} = data.wind;
-       console.log(name,icon,description,temp,humidity,speed);
        document.querySelector(".city").innerText = "Weather in "+ name;
        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+icon+".png" 
        document.querySelector(".wind").innerText = "Wind speed: "+speed+"Km/h";
        document.querySelector(".humidity").innerText = "Humidity: "+humidity+"%";
        document.querySelector(".temp").innerText = temp+"°C";
        document.querySelector(".description").innerHTML = description;
-    }
+       document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+name+"')";
+    },
+    
+    getCity : function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
 };
 
+
+
+document.querySelector(".search button").addEventListener("click",function(){
+    weather.getCity();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup",function(event){
+    if(event.key === "Enter"){
+        weather.getCity();
+    }
+})
+
+weather.fetchWeather("Bangalore");
