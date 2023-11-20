404ec98cd7b306157247bf1382e51cc42496e662
arunraj3 <arunrajgovindan2002@gmail.com>
2023-11-21 03:42:00 +0530

    added env file

--- script.js
+++ script.js
@@ -1,45 +1,48 @@
+const API_KEY = process.env.API_KEY;
 let weather = {
-    "apiKey" : "3c7f6ac6012b1608d90dec2ee7406004",
-    fetchWeather : function(city){
-        fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q="
-            + city
-            + "&units=metric&appid="
-            + weather.apiKey
-            )
-            .then((response) => response.json())
-            .then((data) => this.displayWeather(data));
-            
-    },
-    displayWeather : function(data){
-       const {name} = data;
-       const {description,icon} = data.weather[0];
-       const {temp,humidity} = data.main;
-       const {speed} = data.wind;
-       document.querySelector(".city").innerText = "Weather in "+ name;
-       document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+icon+".png" 
-       document.querySelector(".wind").innerText = "Wind speed: "+speed+"Km/h";
-       document.querySelector(".humidity").innerText = "Humidity: "+humidity+"%";
-       document.querySelector(".temp").innerText = temp+"°C";
-       document.querySelector(".description").innerHTML = description;
-       document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?"+name+"')";
-    },
-    
-    getCity : function(){
-        this.fetchWeather(document.querySelector(".search-bar").value);
-    },
+  apiKey: API_KEY,
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        weather.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { description, icon } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + "Km/h";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".description").innerHTML = description;
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+
+  getCity: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
 };
 
-
-
-document.querySelector(".search button").addEventListener("click",function(){
-    weather.getCity();
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.getCity();
 });
 
-document.querySelector(".search-bar").addEventListener("keyup",function(event){
-    if(event.key === "Enter"){
-        weather.getCity();
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+      weather.getCity();
     }
-})
+  });
 
 weather.fetchWeather("Bangalore");
